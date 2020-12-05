2f8dece2036721628643101325810d1430f9757f
flofiefly <74911778+flofiefly@users.noreply.github.com>
2020-12-05 14:19:34 +0200

    Update Construction_de_mots.py

--- Construction_de_mots.py
+++ Construction_de_mots.py
@@ -1,17 +1,24 @@
-#  - Construction de mots
+'''Construction de mots'''
+
 def generer_dico(nf) :
-    res = []
-    for ligne in nf :
-        res.append(ligne)
+    res = nf.read().splitlines()
     return(res)
 
+def mot_jouable(mot, ll, en_table = {}) :
+#on verifie que mot ne derange pas le tableau
+    k = 0
+    while k < len(en_table) :
+        if k in en_table :
+            if en_table[k] != list(mot)[k] :
+                return(False)
+        k += 1
 
-def mot_jouable(mot, ll, en_table) :
+#on verifie que mot peut s'écrire avec les lettre dans le tableau et les jetons
     temp = []
     for i in ll :
         temp.append(i)
-    for i in en_table :
-        temp.append(i)
+    for k in en_table :
+        temp.append(en_table[k])
     for i in list(mot) :
         if i in temp :
             temp.remove(i)
@@ -20,9 +27,30 @@ def mot_jouable(mot, ll, en_table) :
         else :
             return(False)
     return(True)
-def mots_jouable(motsfr, ll) :
+
+def mots_jouable(motsfr, ll, en_table = {}) :
     res = []
     for i in motsfr :
-        if mot_jouable(i, ll) :
+        if mot_jouable(i, ll, en_table) == True :
             res.append(i)
     return(res)
+
+#test de la fonction 'generer_dico'
+print('test_1 :')
+with open('littre.txt', 'r') as nf :
+    dico = generer_dico(nf)
+    for i in range(10) :
+        print(dico[i])
+        
+#test de la fonction 'mot_jouable'
+print('test_2 :')
+mot = 'INFORMATIQUE'
+en_table = {0:'I', 3:'O', 11:'E'}
+ll = ['?', '?', '?','?', '?', '?','Q', 'R', 'M']
+print(mot_jouable(mot, ll, en_table))
+
+#test de la fonction 'mots_jouable'
+print('test_3 :')
+ll = ['A', 'A', 'B', 'C', 'A']
+en_table = {}
+print(mots_jouable(dico, ll, en_table))
