0bbe0d4c4f8c44fd3a74a38ee04a52bfe9a32332
flofiefly <74911778+flofiefly@users.noreply.github.com>
2020-12-07 17:14:45 +0200

    Update valeur_de_mot.py

--- valeur_de_mot.py
+++ valeur_de_mot.py
@@ -19,10 +19,16 @@ def meilleurs_mots(motsfr, ll, dico) :
     mj = mots_jouables(motsfr, ll)
     if mj == [] :
         return([])
-    res = [mj[0]]
+    res = ['']
     for m in mj :
-        if valeur_mot(m, dico) > valeur_mot(res, dico) :
+        if valeur_mot(m, dico) > valeur_mot(res[0], dico) :
             res = [m]
-        elif valeur_mot(m, dico) == valeur_mot(res, dico) :
+        elif valeur_mot(m, dico) == valeur_mot(res[0], dico) :
             res.append(m)
     return(res)
+
+#test :
+dico = init_dico()
+print(valeur_mot('ABACA',dico))
+print(meilleur_mot(['AA', 'AB', 'AC'], ['A', 'B', 'C'], dico))
+print(meilleurs_mots(['AA', 'AB', 'AC'], ['A', 'B','C'], dico))
