272241198f228212b6a0004e79c9fcb296999b44
flofiefly <74911778+flofiefly@users.noreply.github.com>
2020-12-07 15:46:46 +0200

    Update valeur_de_mot.py

--- valeur_de_mot.py
+++ valeur_de_mot.py
@@ -1,1 +1,28 @@
-
+'''valeur d'un mot '''
+def valeur_mot(mot, dico) :
+    res = 0
+    for l in list(mot) :
+        res += dico[l]['valeur']
+    if len(mot) == 7 :
+        res += 50
+    return(res)
+def meilleur_mot(motsfr, ll, dico) :
+    mj = mots_jouables(motsfr, ll)
+    if mj == [] :
+        return('')
+    res = mj[0]
+    for m in mj :
+        if valeur_mot(m, dico) > valeur_mot(res, dico) :
+            res = m
+    return(res)
+def meilleurs_mots(motsfr, ll, dico) :
+    mj = mots_jouables(motsfr, ll)
+    if mj == [] :
+        return([])
+    res = [mj[0]]
+    for m in mj :
+        if valeur_mot(m, dico) > valeur_mot(res, dico) :
+            res = [m]
+        elif valeur_mot(m, dico) == valeur_mot(res, dico) :
+            res.append(m)
+    return(res)
